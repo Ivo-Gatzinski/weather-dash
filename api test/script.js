5a2ad199a830fb1d977a8d0e2b90c60b34596018
Ezhko <ucsd.camp.21@gmail.com>
2021-09-30 22:10:06 -0400

    added second weather api for city

--- api test/script.js
+++ api test/script.js
@@ -1,30 +1,57 @@
-var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall';
+var cityCall = "https://api.openweathermap.org/data/2.5/weather"
+
+var oneCall = 'https://api.openweathermap.org/data/2.5/onecall';
 
 var apiKey = "112bf445259e35c8b97e82d67527af29"
 
-var lat = 41;
-var lon = 33;
+var lat = 32;
+var lon = -117;
+
+var city = "San Diego";
 
 var responseText = document.getElementById('response-text');
 
-function getApi(url) {
-  fetch(url)
-    .then(function (response) {
-      console.log(response);
-      // display the status
+function getCityApi(url) {
+    fetch(url)
+      .then(function (response) {
+        console.log(response);
+        // display the status
+  
+        responseText.textContent = response.status;
+        // check the response status for success
+        if (response.status === 200) {
+          responseText.style.color = 'green';
+        } else {
+          responseText.style.color = 'red';
+        }
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      });
+  }
 
-      responseText.textContent = response.status;
-      // check the response status for success
-      if (response.status === 200) {
-        responseText.style.color = 'green';
-      } else {
-        responseText.style.color = 'red';
-      }
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
-}
+getCityApi(cityCall + "?q=" + city + "&appid=" + apiKey)
 
-getApi(requestUrl + "?lat=" + lat + "&lon=" + lon +"&appid=" + apiKey);
+
+function getOneApi(url) {
+    fetch(url)
+      .then(function (response) {
+        console.log(response);
+        // display the status
+  
+        responseText.textContent = response.status;
+        // check the response status for success
+        if (response.status === 200) {
+          responseText.style.color = 'green';
+        } else {
+          responseText.style.color = 'red';
+        }
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      });
+  }
+  
+  getOneApi(oneCall + "?lat=" + lat + "&lon=" + lon +"&appid=" + apiKey);
