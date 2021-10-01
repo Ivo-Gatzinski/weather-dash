e3307b332a0c56ec69aef01966cb32e18531c4f9
Ezhko <ucsd.camp.21@gmail.com>
2021-10-01 17:38:28 -0400

    fix form functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,51 @@
-$(".search-button").on("click", function () {
+//ok to use with one city submission only
+
+
+
+$(".search-button").on("click", function (event) {
+    event.preventDefault();
     var input = $(this).siblings(".form-control");
     var name = input.val();
     localStorage.setItem("city", name);
     console.log(name);
   });
 
+
+
+// Get city input
+
+// document.querySelector("input").value();
+
+// store input in local storage
+// attachh city input to first api call
+// get lon and lat from first api call
+// attach lon and lat to second api call
+// 
+
+// Part A:
+// DISPLAY ONE CITY FORECAST
+
+// THINK OF THE CONCEPTUAL FUNCTIONALITY
+
+// Look at TODOS exercisee
+
+
+
+// Part B:
+
+// Search for a city and display forecast
+
+// add parameter for IMPERIAL units
+
+// Part C:
+// let array for searchHistory
+//
+// read array from end of array
+//
+// create and append element
+// 
+// this sorts buttons from recent
+// 
+// save items to end of array in localStorage
+// 
+// bonus: clear history button
