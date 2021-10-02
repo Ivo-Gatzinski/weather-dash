48f8e52ca5fa1f9d7dfede140fc95329eb9a5aec
Ezhko <ucsd.camp.21@gmail.com>
2021-10-02 15:25:26 -0400

    add javascript for history buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 var apiKey = "112bf445259e35c8b97e82d67527af29";
 var searchHistory = [];
-var form = $("search-form");
-var searchInput = $("inputCity");
-var historySearch = $("historySearch");
-var todayForecast = $("today-forecast");
-var fiveDays = $("five-days");
+var form = $(".search-form");
+var searchInput = $(".form-control");
+var historySearch = $(".historySearch");
+var todayForecast = $(".today-forecast");
+var fiveDays = $(".five-days");
 
 //init search history
 searchHistory = localStorage.getItem("search-history");
@@ -20,9 +20,9 @@ function handleFormSubmit(event) {
   event.preventDefault();
   var query = searchInput.val().trim();
   if (query) {
-    searchWeather(query);
-    searchInput.val("");
+    // searchWeather(query);
     addSearchToHistory(query);
+    searchInput.val("");
   }
 }
 
@@ -30,65 +30,80 @@ function handleFormSubmit(event) {
 
 form.on("submit", handleFormSubmit);
 
-// submit city from history
-
-historySearch.on("click", ".search-button", handleSearchClick);
-
-//pass to search API
-
-function handleSearchClick() {
-  searchApi(this.textContent);
-}
-
-//search and display functions:
-
-function searchApi(query) {
-  var requestUrl =
-    "hhttps://api.openweathermap.org/data/2.5/onecall" +
-    "&q=" +
-    query +
-    "&units=Imperial" +
-    "&appid=" +
-    apiKey;
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      resultsContainer.empty();
-      for (var i = 0; i < data.data.length; i++) {
-        displayCurrent(data.data[i]);
-      }
-    });
-}
-
-function displayCurrent(giphyResult) {
-  var imgUrl = giphyResult.images.downsized_large.url;
-  var title = giphyResult.title;
-
-  var img = $("<img>").attr({
-    src: imgUrl,
-    class: "img-fluid",
-    alt: title,
-  });
-  var col = $("<div>").addClass("col-12 col-lg-6 pb-4").append(img);
-
-  resultsContainer.append(col);
-}
+// display history buttons 
 
 function displayButtons() {
-  searchHistoryContainer.empty();
-  // loop over searchHistory
-  for (var i = searchHistory.length - 1; i >= 0; i--) {
-    var button = $("<button>")
-      .attr({
-        type: "button",
-        class: "btn btn-outline-secondary btn-block btn-search",
-      })
-      .text(searchHistory[i]);
-    searchHistoryContainer.append(button);
+    historySearch.empty();
+    // loop over searchHistory
+    for (var i = searchHistory.length - 1; i >= 0; i--) {
+      //create button element to add
+        var addButton = $(".historySearch")
+        .children.attr({
+          type: "button",
+          class: "btn btn-outline-success btn-block",
+        })
+        .text(searchHistory[i]);
+        // append button element
+      historySearch.append(addButton);
+    }
   }
-}
+  
+  // add city to search history in local storage
+  
+  function addSearchToHistory(query) {
+      searchHistory.push(query);
+      localStorage.setItem("search-history", JSON.stringify(searchHistory));
+      displayButtons();
+    }
+
+// // submit city from history
+
+// historySearch.on("click", ".search-button", handleSearchClick);
+
+// //pass to search API
+
+// function handleSearchClick() {
+//   searchApi(this.textContent);
+// }
+
+// //search and display functions:
+
+// function searchApi(query) {
+//   var requestUrl =
+//     "hhttps://api.openweathermap.org/data/2.5/onecall" +
+//     "&q=" +
+//     query +
+//     "&units=Imperial" +
+//     "&appid=" +
+//     apiKey;
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       resultsContainer.empty();
+//       for (var i = 0; i < data.data.length; i++) {
+//         displayCurrent(data.data[i]);
+//       }
+//     });
+// }
+
+// function displayCurrent(giphyResult) {
+//   var imgUrl = giphyResult.images.downsized_large.url;
+//   var title = giphyResult.title;
+
+//   var img = $("<img>").attr({
+//     src: imgUrl,
+//     class: "img-fluid",
+//     alt: title,
+//   });
+//   var col = $("<div>").addClass("col-12 col-lg-6 pb-4").append(img);
+
+//   fiveDays.append(col);
+// }
+
+
+
 
 // USE DATA ATTRIBUTES for buttons
 
